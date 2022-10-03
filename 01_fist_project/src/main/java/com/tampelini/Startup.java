321ad7e039f691754d795f74f8ed04219c0fd066
Leonardo Garcia Tampelini <tampelini@gmail.com>
2022-10-03 14:06:15 -0300

    fisrt steps with java springboot

--- 01_fist_project/src/main/java/com/tampelini/Startup.java
+++ 01_fist_project/src/main/java/com/tampelini/Startup.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.tampelini;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
