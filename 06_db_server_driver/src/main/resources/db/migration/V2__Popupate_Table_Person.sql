538a16f481039b2822e17e3013cf63a9c503b1ac
Leonardo Garcia Tampelini <tampelini@gmail.com>
2022-11-16 00:28:21 -0300

    add exemple hateoas

---
+++ 06_db_server_driver/src/main/resources/db/migration/V2__Popupate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+	(1, 'Ana', 'Clara', 'Rua Voluntários da Pátria, n. 1705', 'Male'),
+	(2, 'Leonardo', 'Tampelini', 'Rua', 'Male');
